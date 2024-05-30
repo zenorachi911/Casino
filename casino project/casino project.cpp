3ad2c3daeff83c3a3cb68faca8699739332b4782
zenorachi911 <fuzzi123123@gmail.com>
2024-05-30 21:32:07 +0300

    with roulette, but without animation

--- casino project/casino project.cpp
+++ casino project/casino project.cpp
@@ -1,12 +1,14 @@
 ﻿#include "header.h"
 
-class Roulette;
+
 
 class Games;
 
 class Menu;
 
 class Bets;
+
+class Roulette;
 
 class Chips
 {
@@ -38,14 +40,6 @@ protected:
     vector<pair<string, int>> chipscol{}; 
 
 public:
-
-    auto operator () (string &b)
-    {
-        Chips a;
-        for (auto& ex : a.chipscol)
-            return ex.first == b;
-    }
-
 
     int chipschange() {
         cin >> moneys;
@@ -114,7 +108,8 @@ public:
         return 0;
     }
 
-    void GetChips() {
+    void GetChips() 
+    {
         for (const auto& chip : chipscol) {
             cout << chip.first << " " << chip.second << endl;
         }
@@ -135,6 +130,259 @@ int Chips::getValue(const string& chipType) {
     else return 0;
 }
 
+class Roulette
+{
+private:
+
+    friend Menu;
+
+    class Bets
+    {
+    public:
+
+        void inter()
+        {
+            cout <<
+                " __________________________________________________\n"
+                "/                                                  \\\n"
+                "|                                                   |\n"
+                "|      | 3 6 9 12 |15 18 21 24|27 30 33 36|         |\n"
+                "|    0 | 2 5 8 11 |14 17 20 23|26 29 32 35|         |\n"
+                "|      | 1 4 7 10 |13 16 19 22|25 28 31 34|         |\n"
+                "|   _____________________________________________   |\n"
+                "|  |   |  1st 12  |  2nd 12   |   3d 12   |      |  |\n"
+                "|  |___|__________|___________|___________|______|  |\n"
+                "|  | 1 to 18| EVEN | red | black | ODD | 19 to 36|  |\n"
+                "|  |________|______|_____|_______|_____|_________|  |\n"
+                "|                                                   |\n"
+                "|___________________________________________________|\n"
+                "\n";
+
+        }
+    };
+
+
+public:
+
+    void Game(Chips& chipsvalue)
+    {
+
+        Bets roulette;
+        roulette.inter();
+
+        short betchoose;
+
+        short bet{};
+        string chip;
+        
+        string betcolor;
+
+        short col;
+
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<> dis(0, 36);
+
+        const int res = dis(gen);
+            
+        cout << "1 - Ставка на любое число" << endl;
+        cout << "2 - Ставка на красное / черное" << endl;
+        cout << "3 - Сатвка на чет / нечет" << endl;
+        cout << "4 - Ставка на ряды" << endl;
+
+        cin >> betchoose;
+
+        cout << "Введите кол-во фишек и ставку" << endl;
+        cin >> col >> chip;
+
+        switch (betchoose)
+        {
+
+        case (1):
+        {
+
+            cout << "Куда хотите поставить?" << endl;
+            cin >> bet;
+
+            if (bet == res)
+            {
+                for (auto& ex : chipsvalue.chipscol)
+                {
+                    if (ex.first == chip)
+                        ex.second *= 35;
+ 
+                }
+                cout << "Вы выиграли!" << endl;
+            }
+            else
+            {
+                for (auto& ex : chipsvalue.chipscol)
+                {
+                    if (ex.first == chip)
+                        ex.second -= col;
+                }
+                cout << "You Lose :(" << endl;
+            }
+            
+            break;
+        }
+        case(2):
+        {
+            int red[18] = { 1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36 };
+            int black[18] = { 2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35 };
+            
+            cout << "red or black?" << endl;
+
+            cin >> betcolor;
+
+            if (betcolor == "red")
+            {
+
+                for (int i = 0; i < size(red); i++)
+                {
+                    if (res == red[i])
+                    {
+                        for (auto& ex : chipsvalue.chipscol)
+                        {
+                            if (ex.first == chip)
+                                ex.second *= 2;
+
+                        }
+                        cout << "You Win!!!" << endl;
+                    }
+                    
+                }
+
+            }
+            else if (betcolor == "black")
+            {
+                for (int i = 0; i < size(black); i++)
+                {
+                    if (res == black[i])
+                    {
+                        for (auto& ex : chipsvalue.chipscol)
+                        {
+                            if (ex.first == chip)
+                                ex.second *= 2;
+                        }
+                        cout << "You Win!!!" << endl;
+                    }
+                    
+                }
+            }
+            else
+            {
+                for (auto& ex : chipsvalue.chipscol)
+                {
+                    if (ex.first == chip)
+                        ex.second -= col;
+                }
+                cout << "You Lose :(" << endl;
+            }
+            
+            break;
+        }
+        case(3):
+        {
+            cout << "even or odd?" << endl;
+            cin >> betcolor;
+
+            if (betcolor == "even")
+            {
+                
+                if (res % 2 == 0)
+                {
+                    for (auto& ex : chipsvalue.chipscol)
+                    {
+                        if (ex.first == chip)
+                            ex.second *= 2;
+                    }
+                    cout << "You Win!!!" << endl;
+                }
+            }
+            else if (betcolor == "odd")
+            {
+                if (res % 2 == 1)
+                {
+                    for (auto& ex : chipsvalue.chipscol)
+                    {
+                        if (ex.first == chip)
+                            ex.second *= 2;
+                    }
+                    cout << "You Win!!!" << endl;
+                }
+            }
+            else
+            {
+                for (auto& ex : chipsvalue.chipscol)
+                {
+                    if (ex.first == chip)
+                        ex.second -= col;
+                }
+                cout << "You Lose :(" << endl;
+            }
+            
+            break;
+        }
+        case(4):
+        {
+            cout << "1st, 2nd or 3d?" << endl;
+            cin >> betcolor;
+
+            if (betcolor == "1st")
+            {
+                if (res <= 12 && res > 0)
+                {
+                    for (auto& ex : chipsvalue.chipscol)
+                    {
+                        if (ex.first == chip)
+                            ex.second *= 3;
+                    }
+                    cout << "You Win!!!" << endl;
+                }
+            }
+            else if (betcolor == "2nd")
+            {
+                if (res <= 24 && res > 12)
+                {
+                    for (auto& ex : chipsvalue.chipscol)
+                    {
+                        if (ex.first == chip)
+                            ex.second *= 3;
+                    }
+                    cout << "You Win!!!" << endl;
+                }
+            }
+            else if (betcolor == "3d")
+            {
+                if (res <= 36 && res > 24)
+                {
+                    for (auto& ex : chipsvalue.chipscol)
+                    {
+                        if (ex.first == chip)
+                            ex.second *= 3;
+                    }
+                    cout << "You Win!!!" << endl;
+                }
+                
+            }
+            else
+            {
+                for (auto& ex : chipsvalue.chipscol)
+                {
+                    if (ex.first == chip)
+                        ex.second -= col;
+                }
+                cout << "You Lose :(" << endl;
+            }
+            
+            break;
+        }
+        }
+        system("pause");
+        system("cls");
+    }
+    };
 
 class Menu
 {
@@ -144,7 +392,7 @@ private:
     string name;
     
 public:
-    int menu(Chips& chips, Roulette& a) 
+    int menu(Chips& chips) 
     {
         
         if (chips.chipscol.empty())
@@ -157,14 +405,13 @@ public:
         while (1)
         {
 
-            cout << "What game do you want to play?\n";
-
-
+            cout << "Welcome to the menu\n";
 
             cout << "\t\t\t-------------------------------------\n"
                 "\t\t\t| 1. Roulette(in development)       |\n"
                 "\t\t\t| 2. Black Jack(in development)     |\n"
-                "\t\t\t| 3. Exit                           |\n"
+                "\t\t\t| 3. Balance                        |\n"
+                "\t\t\t| 4. Exit                           |\n"
                 "\t\t\t-------------------------------------\n";
 
             cin >> choise;
@@ -174,7 +421,8 @@ public:
             {
             case(1):
             {
-               
+                Roulette roulette;
+                roulette.Game(chips);
                 break;
             }
             case(2):
@@ -183,6 +431,13 @@ public:
                 break;
             }
             case(3):
+            {
+                cout << "Balance:\n";
+                chips.GetChips();
+                cout << endl;
+                break;
+            }
+            case(4):
             {
                 cout << "See you soon" << endl;
 
@@ -194,6 +449,7 @@ public:
                 cout << "Unknown command" << endl;
                 system("PAUSE");
                 system("cls");
+                break;
             }
 
             }
@@ -241,104 +497,10 @@ public:
 };
 
 
-class Roulette
-{
-private:
-
-    class Bets
-    {
-    public:
-
-        void inter()
-        {
-            cout <<
-                " __________________________________________________\n"
-                "/                                                  \\\n"
-                "|                                                   |\n"
-                "|      | 3 6 9 12 |15 18 21 24|27 30 33 36|         |\n"
-                "|    0 | 2 5 8 11 |14 17 20 23|26 29 32 35|         |\n"
-                "|      | 1 4 7 10 |13 16 19 22|25 28 31 34|         |\n"
-                "|   _____________________________________________   |\n"
-                "|  |   |  1st 12  |  2nd 12   |   3d 12   |      |  |\n"
-                "|  |___|__________|___________|___________|______|  |\n"
-                "|  | 1 to 18| EVEN | red | black | ODD | 19 to 36|  |\n"
-                "|  |________|______|_____|_______|_____|_________|  |\n"
-                "|                                                   |\n"
-                "|___________________________________________________|\n"
-                "\n";
-
-        }
-    };
-
-public:
-
-    void Game(Chips& a)
-    {
-
-        Bets roulette;
-        roulette.inter();
-
-
-
-        short bet;
-        short col;
-        string chip;
-
-        cout << "Сколько хотите поставить?" << endl;
-
-        cin >> col >> chip;
-
-        cout << "Where do you want to put it?" << endl;
-
-        cin >> bet;
-
-
-        random_device rd;
-        mt19937 gen(rd());
-        uniform_int_distribution<> dis(0, 36);
-
-
-
-        if (bet == dis(gen))
-        {
-            for (auto& ex : a.chipscol)
-            {
-                cout << "You win!!" << endl;
-                if (ex.first == chip)
-                {
-                    ex.second += ex.second * 35;
-                }
-            }
-        }
-        else
-        {
-            cout << "You lose" << endl;
-            for (auto& ex : a.chipscol)
-            {
-
-                if (ex.first == chip)
-                {
-                    ex.second -= col;
-                }
-                
-            }
-
-            cout << dis(gen) << endl;
-        }
-    }
-
-};
-
-    
-
-
-
-
-int main() 
+int main()
 {
     setlocale(LC_ALL, "RU");
 
-    
 
     Roulette a;
 
@@ -350,19 +512,8 @@ int main()
 
     casino.chipschange();
 
-
-    /*console.menu(casino, a);*/
+    console.menu(casino);
 
     a.Game(casino);
 
-    string chipbalance;
-    cin >> chipbalance;
-
-    if (chipbalance == "balance") {
-        casino.GetChips();
-        cout << endl;
-    }
-
-    
-    
 }
